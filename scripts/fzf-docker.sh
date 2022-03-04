f2838188707c2791080d6a6f6a0199d515888fb2
Fernando Guerrero <devrrior@gmail.com>
2022-03-04 13:53:53 -0600

    add docker script for tmux

--- scripts/fzf-docker.sh
+++ scripts/fzf-docker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-dp_commands=`echo "build up down" | tr ' ' '\n'`
+dp_commands=`echo "build up down restart" | tr ' ' '\n'`
 
 selected=$(printf "$dp_commands" | fzf --prompt="docker-compose " -m -1 --reverse --height 50%)
 
